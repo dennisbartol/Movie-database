2133fd2239653932608c6d384235807e8ed78a13
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-11-13 14:53:02 +0100

    Add files via upload

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,60 +1,60 @@
 <template>
-  <div class="movie-detail">
-    <h2>{{movie.Title}}</h2>
+    <div class="movie-data">Detail {{ $route.params.id }}</div>
+    <h2>{{ movie.Title }}</h2>
     <p>{{ movie.Year }}</p>
     <img :src="movie.Poster" alt="Movie Poster" class="featured-img" />
     <p>{{ movie.Plot }}</p>
-  </div>
 </template>
 
-<script>
-import { ref, onBeforeMount } from 'vue';
-import { useRoute } from 'vue-router';
-import env from '@/env.js';
+<script> 
+import { ref, onBeforeMount } from 'vue'; 
+import { useRoute } from 'vue-router'; 
+import env from '@env.js';
 
 export default {
-  setup () {
-    const movie = ref({});
-    const route = useRoute();
+    setup() {
+        const movie = ref({});
+        const route = useRoute();
 
-    onBeforeMount(() => {
-      fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
-        .then(response => response.json())
-        .then(data => {
+        onBeforeMount(() => {
+          fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+            .then(response => response.json())
+            .then(data => {
           movie.value = data;
         });
     });
+    
 
     return {
-      movie
+        movie
     }
   }
 }
 </script>
 
 <style lang="scss">
+
 .movie-detail {
   padding: 16px;
 
   h2 {
-    color: #FFF;
-    font-size: 28px;
+    color: ghostwhite;
+    font-size: 24px;
     font-weight: 600;
-    margin-bottom: 16px;
+    margin-bottom: 14px;
   }
 
   .featured-img {
     display: block;
     max-width: 200px;
     margin-bottom: 16px;
-	margin: auto;
-	
   }
 
   p {
-    color: #FFF;
+    color: ghostwhite;
     font-size: 18px;
     line-height: 1.4;
   }
 }
+
 </style>
