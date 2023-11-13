b17d7c9085512e72e0e187f032f7e78530f774f8
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-11-13 15:09:18 +0100

    Update MovieDetail.vue

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,58 +1,215 @@
 <template>
-  <div class="movie-detail">
-    <h2>{{movie.Title}}</h2>
-    <p>{{ movie.Year }}</p>
-    <img :src="movie.Poster" alt="Movie Poster" class="featured-img" />
-    <p>{{ movie.Plot }}</p>
+  <div class="home">
+    <div class="feature-card">
+      <router-link to="/movie/tt0086190">
+        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/773B031A52B5727F7C218E42149B45095C21A8BA56601EB1F51FC46485304210/scale?width=1200&aspectRatio=1.78&format=jpeg" alt="Naruto Poster" class="featured-img" />
+        <div class="detail">
+          <h3>Star Wars - Return of the Jedi</h3>
+          <p>After rescuing Han Solo from Jabba the Hutt, the Rebels 
+             attempt to destroy the second Death Star, while Luke 
+             struggles to help Darth Vader back from the dark side.</p>
+        </div>
+      </router-link>
+    </div>
+
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input type="text" placeholder="Please type in your search ..." v-model="search" />
+      <input type="submit" value="Search" />
+    </form>
+
+    <div class="movies-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref, onBeforeMount } from 'vue';
-import { useRoute } from 'vue-router';
-import env from '@/env.js';
+import { ref } from 'vue';
+import env from '@/env.js'
 
 export default {
   setup () {
-    const movie = ref({});
-    const route = useRoute();
+    const search = ref("");
+    const movies = ref([]);
 
-    onBeforeMount(() => {
-      fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
-        .then(response => response.json())
-        .then(data => {
-          movie.value = data;
-        });
-    });
+    const SearchMovies = () => {
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+          .then(response => response.json())
+          .then(data => {
+            movies.value = data.Search;
+            search.value = "";
+          });
+      }
+    }
 
     return {
-      movie
+      search,
+      movies,
+      SearchMovies
     }
   }
 }
 </script>
 
 <style lang="scss">
-.movie-detail {
-  padding: 16px;
+.home {
+  .feature-card {
+    position: relative;
 
-  h2 {
-    color: #FFF;
-    font-size: 28px;
-    font-weight: 600;
-    margin-bottom: 16px;
+    .featured-img {
+      display: block;
+      width: 100%;
+
+      height: 300px;
+      object-fit: cover;
+
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color:#FFF;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #FFF;
+      }
+    }
   }
 
-  .featured-img {
-    display: block;
-    max-width: 200px;
-    margin-bottom: 16px;
+  .search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 14px;
+
+    input {
+      display: block;
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+
+      &[type="text"] {
+        width: 50%;
+		max-width: 600px;
+		color: ghostwhite;
+        background-color: #486583;
+        font-size: 20px;
+        padding: 10px 16px;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        transition: 0.4s;
+
+        &::placeholder {
+          color: ghostwhite;
+        }
+
+
+        &:focus {
+          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+        }
+		
+      }
+
+      &[type="submit"] {
+        width: 100%;
+        max-width: 280px;
+        background-color: #42B883;
+        padding: 14px;
+        border-radius: 6px;
+		cursor: pointer;
+        color: ghostwhite;
+        font-size: 20px;
+        text-transform: uppercase;
+        transition: 0.4s;
+
+        &:active {
+          background-color: #222;
+        }
+      }
+    }
   }
 
-  p {
-    color: #FFF;
-    font-size: 18px;
-    line-height: 1.4;
+  .movies-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+
+    .movie {
+      max-width: 400px;
+      flex: 1 1 400px;
+      padding: 14px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+          display: block;
+
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42B883;
+            color: ghostwhite;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+
+        .detail {
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+
+          .year {
+            color: #AAA;
+            font-size: 14px;
+          }
+
+          h3 {
+            color: ghostwhite;
+            font-weight: 600;
+            font-size: 18px;
+          }
+        }
+      }
+    }
   }
 }
 </style>
